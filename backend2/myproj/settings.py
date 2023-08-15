27638a8e331f571f052c46e94f36f9ed93bb8600
yoni <orenyoni87@gmail.com>
2023-08-15 22:01:42 +0300

    fix

--- backend2/myproj/settings.py
+++ backend2/myproj/settings.py
@@ -98,8 +98,8 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
