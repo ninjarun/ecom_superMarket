bdf1a4d3c3d2aceb7ea1621821ec48114c476794
yoni <orenyoni87@gmail.com>
2023-08-17 21:26:54 +0300

    fix for serving statics when debug false

--- backend2/myproj/urls.py
+++ backend2/myproj/urls.py
@@ -24,7 +24,11 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+
+# This will always serve media and static files using Django, regardless of the DEBUG setting.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
