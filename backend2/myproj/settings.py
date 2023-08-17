f74410f589050431922aea330cb78e0b93367c72
yoni <orenyoni87@gmail.com>
2023-08-17 22:28:22 +0300

    fixing

--- backend2/myproj/settings.py
+++ backend2/myproj/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9t1pstl0+b$+*zr*2*typ5^bhi208(ewp8z-+)h1lr+aqe+9e0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -194,11 +194,11 @@ CORS_ALLOW_ALL_ORIGINS = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    # BASE_DIR / 'frontend/build/static'
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+#     # BASE_DIR / 'frontend/build/static'
+# ]
 
 
 
--- backend2/myproj/urls.py
+++ backend2/myproj/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -30,5 +31,8 @@ urlpatterns = [
 
 
 # This will always serve media and static files using Django, regardless of the DEBUG setting.
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
